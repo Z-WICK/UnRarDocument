d041d903942ebddc0723a734a6a35bc2f1b3cf5b
wick <2289431216@qq.com>
2022-03-24 15:32:41 +0800

    新增解压tar格式

--- UnDocument.py
+++ UnDocument.py
@@ -1,16 +1,19 @@
+from Unzipped_script.tar import Untar
 from Unzipped_script.zip import *
 from Unzipped_script.rar import *
 
 
 def choose_fun():
-        try:
-            print("1.rar"
-                  "\n2.zip"
-                  "\n3.我都要")
-            choose_fun = int(input("输入你需要的解压的格式："))
-            return choose_fun
-        except ValueError:
-            print("输入的值有误")
+    try:
+        print("1.我都要"
+              "\n2.zip"
+              "\n3.rar"
+              "\n4.tar")
+        choose_fun = int(input("输入你需要的解压的格式："))
+        return choose_fun
+    except ValueError:
+        print("输入的值有误")
+
 
 def Unzip_parameters():
     base_path = input("输入需要解压的路径：")
@@ -22,18 +25,24 @@ def Unzip_parameters():
 while True:
     function_num = choose_fun()
 
-    if function_num == 1:       # 解压RAR
-        Un_param = Unzip_parameters()
-        Unrar(Un_param[0], Un_param[1], Un_param[2])
-
-    elif function_num == 2:     # 解压ZIP
-        Un_param = Unzip_parameters()
-        UnZip(Un_param[0], Un_param[1], Un_param[2])
-
-    elif function_num == 3:    #我都要
+    if function_num == 1:  # 我都要
         Un_param = Unzip_parameters()
         Unrar(Un_param[0], Un_param[1], Un_param[2])
         UnZip(Un_param[0], Un_param[1], Un_param[2])
+        Untar(Un_param[0], Un_param[1], Un_param[2])
+
+
+    elif function_num == 2:  # 解压ZIP
+        Un_param = Unzip_parameters()
+        UnZip(Un_param[0], Un_param[1], Un_param[2])
+
+    elif function_num == 3:  # 解压RAR
+        Un_param = Unzip_parameters()
+        Unrar(Un_param[0], Un_param[1], Un_param[2])
+
+    elif function_num == 4:  # tar
+        Un_param = Unzip_parameters()
+        Untar(Un_param[0], Un_param[1], Un_param[2])
 
     ifContinue = input("是否继续解压？y/n:")
     if ifContinue == "n":
---
+++ Unzipped_script/tar.py
@@ -0,0 +1,29 @@
+import os
+import tarfile
+
+
+def Untar(base_path, unPathPrefix, ifDelete):
+    files = os.listdir(base_path)
+    files.sort()
+    for path in files:
+        full_path = os.path.join(base_path, path)
+        print("正在解压------->>>>" + path)
+        try:
+            z = tarfile.TarFile(full_path)
+            UnTargetPath = base_path + "/UnTar-" + unPathPrefix + "/" + path[0:-4]
+            z.extractall(UnTargetPath)
+            z.close()
+        except IsADirectoryError:
+            print("该文件不需要解压")
+            continue
+        except Exception:
+            print(path[0:-4] + "该文件有误")
+            print("该文件可能是zip等其他格式的压缩文件")
+            continue
+
+        if ifDelete == "y":
+            try:
+                os.remove(full_path)  # 删除源文件
+                print("源文件-" + path[0:-4] + "\n已删除")
+            except Exception:
+                print("文件异常")
